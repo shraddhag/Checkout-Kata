37d6819969a4b3b197e25d373dc79093ed86badf
Shraddha G <shraddha0206@gmail.com>
2020-08-08 22:15:09 +0100

    Added code to calculate cash promotion. Added unit tests

---
+++ CheckoutKata/CheckoutKata.Tests/MultiBuyCashPromotionRuleTests.cs
@@ -0,0 +1,69 @@
+﻿using CheckoutKata.PromotionRules.MultiBuyPromotionRules;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CheckoutKata.Tests
+{
+    [TestClass]
+    public class MultiBuyCashPromotionRuleTests
+    {
+        decimal totalPromotion = 0m;
+
+        [TestMethod]
+        public void CalculateMultiBuyCashPromotion_WithZeroPromotionItemQuantity()
+        {
+            //Arrange            
+            var itemB = new Item { SKU = "B", UnitPrice = 15, Quantity = 1, Discount = DiscountType.MultiBuyCash };
+            MultiBuyCashPromotionRule cashPromotionRule = new MultiBuyCashPromotionRule("B", 0, 40.00m);
+
+            //Act
+            totalPromotion = cashPromotionRule.CalculateMultiBuyItemPromotion(itemB);
+
+            //Assert
+            Assert.AreEqual(0.00m, totalPromotion);
+        }
+
+        [TestMethod]
+        public void CalculateMultiBuyCashPromotion_With1Items()
+        {
+            //Arrange            
+            var itemB = new Item { SKU = "B", UnitPrice = 15, Quantity = 1, Discount = DiscountType.MultiBuyCash };
+            MultiBuyCashPromotionRule cashPromotionRule = new MultiBuyCashPromotionRule("B", 3, 40.00m);
+
+            //Act
+            totalPromotion = cashPromotionRule.CalculateMultiBuyItemPromotion(itemB);
+
+            //Assert
+            Assert.AreEqual(0.00m, totalPromotion);
+        }
+
+        [TestMethod]
+        public void CalculateMultiBuyCashPromotion_With3Items()
+        {
+            //Arrange            
+            var itemB = new Item { SKU = "B", UnitPrice = 15, Quantity = 3, Discount = DiscountType.MultiBuyCash };
+            MultiBuyCashPromotionRule cashPromotionRule = new MultiBuyCashPromotionRule("B", 3, 40.00m);
+
+            //Act
+            totalPromotion = cashPromotionRule.CalculateMultiBuyItemPromotion(itemB);
+
+            //Assert
+            Assert.AreEqual(5.00m, totalPromotion);
+        }
+
+        [TestMethod]
+        public void CalculateMultiBuyCashPromotion_With7Items()
+        {
+            //Arrange
+            var itemB = new Item { SKU = "B", UnitPrice = 15, Quantity = 7, Discount = DiscountType.MultiBuyCash };
+
+            MultiBuyCashPromotionRule cashPromotionRule = new MultiBuyCashPromotionRule("B", 3, 40.00m);
+
+            //Act
+            totalPromotion = cashPromotionRule.CalculateMultiBuyItemPromotion(itemB);
+
+            //Assert
+            Assert.AreEqual(10.00m, totalPromotion);
+        }
+    }
+}
+
---
+++ CheckoutKata/CheckoutKata/PromotionRules/MultiBuyPromotionRules/MultiBuyCashPromotionRule.cs
@@ -0,0 +1,42 @@
+﻿
+namespace CheckoutKata.PromotionRules.MultiBuyPromotionRules
+{
+    public class MultiBuyCashPromotionRule
+    {
+        public string SKU { get; set; }
+        public int PromotionItemQuantity { get; set; }
+        public decimal PromotionPrice { get; set; }
+
+        public MultiBuyCashPromotionRule(string sku, int promotionItemQuantity, decimal promotionPrice)
+        {
+            SKU = sku;
+            PromotionItemQuantity = promotionItemQuantity;
+            PromotionPrice = promotionPrice;
+        }
+
+        public decimal CalculateMultiBuyItemPromotion(Item item)
+        {
+            decimal totalPromotion = 0m;
+
+            if (item.SKU == SKU && item.Discount == DiscountType.MultiBuyCash)
+            {
+                if (PromotionItemQuantity > 0 && item.Quantity >= PromotionItemQuantity)
+                {
+                    var perLotPromotion = (item.UnitPrice * PromotionItemQuantity) - PromotionPrice;
+                    var totalDiscountedItemQuantity = item.Quantity - (item.Quantity % PromotionItemQuantity);
+
+                    if (totalDiscountedItemQuantity > PromotionItemQuantity)
+                    {
+                        totalPromotion = perLotPromotion * (totalDiscountedItemQuantity / PromotionItemQuantity);
+                    }
+                    else
+                    {
+                        totalPromotion = perLotPromotion;
+                    }
+                }
+            }
+
+            return totalPromotion;
+        }
+    }
+}
